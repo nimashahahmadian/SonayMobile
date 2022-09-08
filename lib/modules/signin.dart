11909145efaa92980bda580558e4e7364054235d
nima <nimashahahmadian@gmail.com>
2022-09-08 23:08:06 +0430

    lesser changes

--- lib/modules/signin.dart
+++ lib/modules/signin.dart
@@ -10,8 +10,89 @@ class signin extends StatefulWidget {
 class _signinState extends State<signin> {
   @override
   Widget build(BuildContext context) {
+    TextEditingController namecont = TextEditingController();
+    TextEditingController passcont = TextEditingController();
+    var wi = MediaQuery.of(context).size.width;
+    var he = MediaQuery.of(context).size.height;
     return Scaffold(
-      body: SafeArea(child: Container()),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            height: he * .6,
+            margin: EdgeInsets.only(top: he * .07),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                fieldbuilder(
+                    wi,
+                    he,
+                    'UserName',
+                    icon: Icons.person,
+                    namecont,
+                    25,
+                    inputtype: TextInputType.text,
+                    obscured: false),
+                fieldbuilder(
+                    wi,
+                    he,
+                    'PassWord',
+                    icon: Icons.lock,
+                    passcont,
+                    50,
+                    inputtype: TextInputType.text,
+                    obscured: true),
+                Container(
+                  constraints:
+                      BoxConstraints(minHeight: he * .09, minWidth: wi * .3),
+                  margin: EdgeInsets.symmetric(horizontal: wi * .18),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.grey,
+                            blurRadius: 10,
+                            offset: Offset(5, 5))
+                      ]),
+                  child: Center(
+                    child: InkWell(
+                      onTap: () {},
+                      child: Text('Enter'),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget fieldbuilder(
+      var wi, var he, String label, TextEditingController cont, int mxlength,
+      {IconData? icon, TextInputType? inputtype, bool obscured = false}) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: wi * .1),
+      child: TextField(
+          maxLength: mxlength,
+          obscureText: obscured,
+          keyboardType: inputtype,
+          textInputAction: TextInputAction.none,
+          controller: cont,
+          cursorColor: Colors.grey,
+          decoration: InputDecoration(
+              prefixIcon: Icon(
+                icon,
+                color: Colors.pink[300],
+              ),
+              labelStyle:
+                  TextStyle(color: Colors.pink[400], fontFamily: 'sans'),
+              label: Text(label),
+              focusedBorder:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(15)),
+              border:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(15)))),
     );
   }
 }
--- lib/modules/signup.dart
+++ lib/modules/signup.dart
@@ -10,8 +10,92 @@ class signup extends StatefulWidget {
 class _signupState extends State<signup> {
   @override
   Widget build(BuildContext context) {
+    TextEditingController namecont = TextEditingController();
+    TextEditingController passcont = TextEditingController();
+    TextEditingController mailcont = TextEditingController();
+    TextEditingController phonecont = TextEditingController();
+    var wi = MediaQuery.of(context).size.width;
+    var he = MediaQuery.of(context).size.height;
     return Scaffold(
-      body: SafeArea(child: Container()),
+      body: SafeArea(
+          child: Container(
+        child: ListView(children: <Widget>[
+          SizedBox(
+            height: he * .08,
+          ),
+          fieldbuilder(wi, he, 'UserName', namecont, 20,
+              icon: Icons.person, inputtype: TextInputType.name),
+          SizedBox(
+            height: he * .05,
+          ),
+          fieldbuilder(wi, he, 'Email', mailcont, 60,
+              icon: Icons.email_outlined,
+              inputtype: TextInputType.emailAddress),
+          SizedBox(
+            height: he * .05,
+          ),
+          fieldbuilder(wi, he, 'Phone Number', phonecont, 11,
+              icon: Icons.phone_android_outlined,
+              inputtype: TextInputType.phone),
+          SizedBox(
+            height: he * .05,
+          ),
+          fieldbuilder(wi, he, 'Password', passcont, 50,
+              icon: Icons.lock_open_outlined,
+              inputtype: TextInputType.name,
+              obscured: true),
+          SizedBox(
+            height: he * .05,
+          ),
+          Container(
+            constraints: BoxConstraints(minHeight: he * .09, minWidth: wi * .3),
+            margin: EdgeInsets.symmetric(horizontal: wi * .18),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.grey, blurRadius: 10, offset: Offset(5, 5))
+                ]),
+            child: Center(
+              child: InkWell(
+                onTap: () {},
+                child: Text('Register'),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: he * .05,
+          ),
+        ]),
+      )),
+    );
+  }
+
+  Widget fieldbuilder(
+      var wi, var he, String label, TextEditingController cont, int mxlength,
+      {IconData? icon, TextInputType? inputtype, bool obscured = false}) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: wi * .1),
+      child: TextField(
+          maxLength: mxlength,
+          obscureText: obscured,
+          keyboardType: inputtype,
+          textInputAction: TextInputAction.none,
+          controller: cont,
+          cursorColor: Colors.grey,
+          decoration: InputDecoration(
+              prefixIcon: Icon(
+                icon,
+                color: Colors.pink[300],
+              ),
+              labelStyle:
+                  TextStyle(color: Colors.pink[400], fontFamily: 'sans'),
+              label: Text(label),
+              focusedBorder:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(15)),
+              border:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(15)))),
     );
   }
 }
